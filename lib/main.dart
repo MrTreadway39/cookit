69888e3831b5e351777a5d44f7e53345d690ed5f
MrTreadway39 <gruia1novac@gmail.com>
2023-04-25 21:38:19 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const CookingApp());
+
+class CookingApp extends StatelessWidget {
+  const CookingApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cooking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        '/how_you_want': (context) => HowYouWantPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cooking App'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/c.jpg")
+            ,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Welcome to the Cooking App!',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/how_you_want');
+                },
+                child: const Text('Start Cooking'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HowYouWantPage extends StatelessWidget {
+  const HowYouWantPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('How You Want'),
+      ),
+      body: const Center(
+        child: Text(
+          'How do you want to cook?',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
